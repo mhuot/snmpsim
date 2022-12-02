05c5eb2babce0348bb6096c5e714d8b2baaf6736
Lex Li <support@lextm.com>
2022-12-02 00:38:52 -0500

    Added sitemap.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,12 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.viewcode']
+extensions = ['sphinx.ext.viewcode',
+    'sphinx_sitemap'
+]
+
+html_baseurl = 'https://www.pysnmp.com/snmpsim'
+sitemap_url_scheme = '{link}'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
